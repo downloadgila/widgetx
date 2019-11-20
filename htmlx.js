d650b830675af41c9df39d354af3b7adda1e4adc
downloadgila <56267929+downloadgila@users.noreply.github.com>
2019-11-21 02:50:03 +0700

    Create htmlx.js

---
+++ htmlx.js
@@ -0,0 +1,7 @@
+//<![CDATA[
+$(document).ready(function () {
+    if ($("#footerprtx,.footerprtx").attr("href") != "https://informasikontes.blogspot.com") {
+        window.location.href = "https://informasikontes.blogspot.com";
+    }
+});
+//]]>
